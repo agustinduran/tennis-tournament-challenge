4efddbfd1b3c53091cfec9e1f7431aaa7e82f32d
Agustín Durán <agustineduran@gmail.com>
2024-06-10 04:52:12 -0300

    feat(player): model

---
+++ src/Entity/Player.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlayerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PlayerRepository::class)]
+class Player
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $fullName = null;
+
+    #[ORM\Column]
+    private ?int $habilityLevel = null;
+
+    #[ORM\Column]
+    private ?int $luckyLevel = null;
+
+    #[ORM\ManyToOne(inversedBy: 'gender')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Gender $gender = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFullName(): ?string
+    {
+        return $this->fullName;
+    }
+
+    public function setFullName(string $fullName): static
+    {
+        $this->fullName = $fullName;
+
+        return $this;
+    }
+
+    public function getHabilityLevel(): ?int
+    {
+        return $this->habilityLevel;
+    }
+
+    public function setHabilityLevel(int $habilityLevel): static
+    {
+        $this->habilityLevel = $habilityLevel;
+
+        return $this;
+    }
+
+    public function getLuckyLevel(): ?int
+    {
+        return $this->luckyLevel;
+    }
+
+    public function setLuckyLevel(int $luckyLevel): static
+    {
+        $this->luckyLevel = $luckyLevel;
+
+        return $this;
+    }
+
+    public function getGender(): ?Gender
+    {
+        return $this->gender;
+    }
+
+    public function setGender(?Gender $gender): static
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+}
