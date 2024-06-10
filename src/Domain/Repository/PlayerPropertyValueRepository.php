ee644d3f6a8c3ae0cc10db541a6a088310a621e2
Agustín Durán <agustineduran@gmail.com>
2024-06-10 06:25:50 -0300

    feat(player): repository

--- src/Domain/Repository/PlayerPropertyValueRepository.php
+++ src/Domain/Repository/PlayerPropertyValueRepository.php
@@ -6,9 +6,6 @@ use App\Domain\Model\PlayerPropertyValue;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<PlayerPropertyValue>
- */
 class PlayerPropertyValueRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
@@ -16,28 +13,9 @@ class PlayerPropertyValueRepository extends ServiceEntityRepository
         parent::__construct($registry, PlayerPropertyValue::class);
     }
 
-    //    /**
-    //     * @return PlayerPropertyValue[] Returns an array of PlayerPropertyValue objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?PlayerPropertyValue
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function save(PlayerPropertyValue $playerPropertyValue): void
+    {
+        $this->_em->persist($playerPropertyValue);
+        $this->_em->flush();
+    }
 }
