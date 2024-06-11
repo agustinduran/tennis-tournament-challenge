5d9bc38a3a375805db7bb62fa83b2d33ae348bc1
Agustín Durán <agustineduran@gmail.com>
2024-06-11 13:44:19 -0300

    feat(migration): update

--- migrations/Version20240611164305.php
+++ migrations/Version20240611164305.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240611143046 extends AbstractMigration
+final class Version20240611164305 extends AbstractMigration
 {
     public function getDescription(): string
     {
