25412ff831ebc63cc151b10290c10c1ec805b716
Agustín Durán <agustineduran@gmail.com>
2024-06-11 15:26:34 -0300

    feat(seed): put gender on player property

--- src/Application/Service/Seed/SeedPlayerPropertyService.php
+++ src/Application/Service/Seed/SeedPlayerPropertyService.php
@@ -3,24 +3,28 @@
 namespace App\Application\Service\Seed;
 
 use App\Domain\Model\PlayerProperty;
+use App\Domain\Repository\GenderRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 class SeedPlayerPropertyService
 {
     private EntityManagerInterface $entityManager;
+    private GenderRepository $genderRepository;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, GenderRepository $genderRepository)
     {
         $this->entityManager = $entityManager;
+        $this->genderRepository = $genderRepository;
     }
 
     public function execute()
     {
-        $properties = ['Fuerza', 'Velocidad de Desplazamiento', 'Tiempo de Reacción'];
+        $properties = [['Fuerza', 'Masculino'], ['Velocidad de Desplazamiento', 'Masculino'], ['Tiempo de Reacción', 'Femenino']];
 
-        foreach ($properties as $name) {
+        foreach ($properties as $valueData) {
             $property = new PlayerProperty();
-            $property->setName($name);
+            $property->setName($valueData[0]);
+            $property->setGender($this->genderRepository->findOneBy(['name' => $valueData[1]]));
             $this->entityManager->persist($property);
         }
         $this->entityManager->flush();
