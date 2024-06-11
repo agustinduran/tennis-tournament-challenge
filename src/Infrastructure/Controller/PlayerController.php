5aa40698c43f0ab6aa258bdfcd731e5688374fdf
Agustín Durán <agustineduran@gmail.com>
2024-06-11 07:09:49 -0300

    fix(player): call class

--- src/Infrastructure/Controller/PlayerController.php
+++ src/Infrastructure/Controller/PlayerController.php
@@ -5,7 +5,7 @@ namespace App\Infrastructure\Controller;
 use App\Application\Service\CreatePlayerService;
 use App\Application\Service\GetPlayersService;
 use App\Application\Service\GetPlayerService;
-use App\Domain\Repository\PlayerRepository;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
